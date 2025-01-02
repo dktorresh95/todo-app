e71e80f7c5e68dda0053481820ddf274261ea487
dktorres1995 <dktorres95@gmail.com>
2025-01-02 10:25:07 -0500

    Se configura jest para el proyecto

--- src/app/tasks/task-create/task-create.component.spec.ts
+++ src/app/tasks/task-create/task-create.component.spec.ts
@@ -1,21 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TaskCreateComponent } from './task-create.component';
+import { TaskService } from 'src/app/services/task.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('TaskCreateComponent', () => {
   let component: TaskCreateComponent;
   let fixture: ComponentFixture<TaskCreateComponent>;
+  let taskService: TaskService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TaskCreateComponent]
+      declarations: [TaskCreateComponent],
+      providers: [
+        TaskService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '1' }),
+          },
+        },
+      ],
     });
     fixture = TestBed.createComponent(TaskCreateComponent);
     component = fixture.componentInstance;
+    taskService = TestBed.inject(TaskService);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create the form with controls', () => {
+    expect(component.taskForm.contains('id')).toBe(true);
+    expect(component.taskForm.contains('completed')).toBe(true);
+    expect(component.taskForm.contains('description')).toBe(true);
+    expect(component.taskForm.contains('title')).toBe(true);
   });
 });
